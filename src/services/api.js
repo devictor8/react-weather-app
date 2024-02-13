8436efbb991bad9e4867fe878aadbbbce5465c49
devictor8 <joaosalgado605@gmail.com>
2024-02-13 16:37:20 -0300

    endpoint da api atualizado

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 const KEY = 'f35a55f259504178842163911241302';
 
 const fetchData = async (city) => {
-  const url = `http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`;
+  const url = `https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`;
 
   const fetchResponse = await fetch(url);
   const data = await fetchResponse.json();
